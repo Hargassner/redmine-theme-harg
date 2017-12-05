1c76acd189e77a1ddf0d2eb8b064d6cdf9c0964c
Michael Schmidt <michael.schmidt@hargassner.at>
2017-12-05 08:52:44 +0100

    .

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,7 +1,8 @@
-
-// zusätzliches div für H-Logo
-var logo = document.createElement('div');
-logo.id = 'h_logo';
-var b = document.getElementsByTagName('body')[0];
-b.appendChild(logo);
-logo.id = 'h_logo';
+$(document).ready(function(){
+  // zusätzliches div für H-Logo
+  var logo = document.createElement('div');
+  logo.id = 'h_logo';
+  var b = document.getElementsByTagName('body')[0];
+  b.appendChild(logo);
+  logo.id = 'h_logo';
+};
