d5fa8e8e5d77ea094bc83e4ce87c87e8a6ff5f02
Michael Schmidt <michael.schmidt@hargassner.at>
2017-12-05 08:22:56 +0100

    .

---
+++ javascripts/theme.js
@@ -0,0 +1,5 @@
+var jQ = document.createElement('script');
+jQ.type = 'text/javascript';
+jQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+//jQ.onload = ;
+document.getElementsByTagName('head')[0].appendChild(jQ);
